51bad6736f2e135b667766dfee5d3272541c7638
Mari Azh <Marzi.aghchay@gmail.com>
2023-02-06 20:33:29 +0000

    some methods

---
+++ script.js
@@ -0,0 +1,22 @@
+// //slice methods in array
+// let arr =['a','b','c','d','e'];
+// console.log(arr.slice()); 
+// console.log(arr.slice(1));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(2,3));
+// console.log(arr.slice(1,-3));
+// console.log(arr.slice(3,-1));
+
+//splice
+
+// let arr =['a','b','c','d','e'];
+// console.log(arr);
+// arr.splice(3);
+// arr.splice(-1);
+// console.log(arr);
+
+//revers 
+// let arr1 =['a','b','c','d','e'];
+// console.log(arr1.reverse());
+
+
